61a5ce0362c0237042792933bdcafde8523c03d5
lucifer89943-sketch <genius.talk.6789@gmail.com>
2026-02-28 13:10:14 +0530

    Adds reverse iteration functionality for text sequences
    Introduces a custom iterator enabling reverse traversal over text.
    Enhances sequence handling flexibility by supporting backward iteration,
    which can simplify certain algorithms and improve code clarity.

--- iterators.py
+++ iterators.py
@@ -49,3 +49,23 @@ for number in counter:
     print(number)
 
 
+#next question:--
+
+class ReverseText:
+    def __init__(self, text):
+        self.text = text
+        self.index = (len(text))
+
+    def __iter__(self):
+        return self
+
+    def __next__ (self):
+        if self.index == 0:
+            raise StopIteration
+        else:
+            self.index -= 1
+            return self.text[self.index]
+
+reverse_text = ReverseText("Python")
+for char in reverse_text:
+    print(char)
